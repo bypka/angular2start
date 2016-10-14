78d8518c2c82811e6dd1e382c2940e81793ee2c4
bypka <burakpavelwork@gmail.com>
2016-10-14 22:34:10 +0300

    1st commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by dzu on 14.10.2016.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My Ruslan and Bypka Angular App</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by dzu on 14.10.2016.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
